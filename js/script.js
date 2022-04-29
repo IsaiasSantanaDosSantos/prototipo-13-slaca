5d3ba37965b4dec4ea996805ebdfa9720a1ef4d6
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-04-29 05:05:11 -0300

    chore: Function to show the sharing session again after 10 seconds of sending the comment

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
+let startDiscussion = document.getElementById("sharing-session")
+let showAndHideComments = document.getElementById("hidden-comments");
+let form = document.getElementById("form-message");
+let confirmShipping = document.getElementById("confirm-shipping");
+
+let hideText = document.getElementById("hideText");
+let showText = document.getElementById("showText");
+
+
 /* Função para mostrar conteúdo do resumo */
 function showContent() {
-  let hideText = document.getElementById("hideText");
-  let showText = document.getElementById("showText");
-
   if (hideText.style.display === "none") {
     hideText.style.display = "inline";
     showText.style.display = "none";
@@ -10,38 +16,38 @@ function showContent() {
     hideText.style.display = "none";
     showText.style.display = "inline";
   }
-}
+} /* sharing-session */
 
 /* Interação do formulário */
 
 function createTopic() {
-  let startDiscussion = document.getElementById("sharing-session");
-  let form = document.getElementById("form-message");
-  let confirmShipping = document.getElementById("confirm-shipping");
-
   startDiscussion.style.display = "none";
   form.style.display = "block";
   confirmShipping.style.display = "none";
 }
 
+/* Voltar campo compartilhamento */
+const initialPosition = () => {
+    startDiscussion.style.display = "block";
+    form.style.display = "none";
+    confirmShipping.style.display = "none";
+  }
+
 /* Inviar pergunta */
-
 function sendPost() {
-  let form = document.getElementById("form-message");
-  let confirmShipping = document.getElementById("confirm-shipping");
-
   form.style.display = "none";
   confirmShipping.style.display = "block";
+  setInterval(initialPosition, 10000);
 }
 
 /* Mostrar comentario */
 
 function showComments() {
-  let showAndHideComments = document.getElementById("hidden-comments");
-
   if (showAndHideComments.style.display === "none") {
     showAndHideComments.style.display = "block";
   } else {
     showAndHideComments.style.display = "none"
   }
 }
+
+
