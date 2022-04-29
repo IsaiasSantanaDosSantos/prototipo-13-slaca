327863f634e3421de820fcaee8828ca7cf45d7d9
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-04-28 22:01:04 -0300

    build: 1/4 and 2/4 part of the discussion session sharing

--- js/script.js
+++ js/script.js
@@ -5,10 +5,19 @@ function showContent() {
 
   if (hideText.style.display === "none") {
     hideText.style.display = "inline";
-    showText.style.display = "none"
+    showText.style.display = "none";
   } else {
     hideText.style.display = "none";
-    showText.style.display = "inline"
+    showText.style.display = "inline";
   }
-};
+}
 
+/* Interação do formulário */
+
+function createTopic() {
+  let startDiscussion = document.getElementById("sharing-session");
+  let form = document.getElementById("form-message");
+  
+  startDiscussion.style.display = "none";
+  form.style.display = "block";
+}
